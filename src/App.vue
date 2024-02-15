c62c2e524dafe59082e6a71fb9a4fc753d3169bf
Tumen Dorzhaev <tumen-dorzhaev@mail.ru>
2024-02-15 21:48:56 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped lang="sass">
+</style>
