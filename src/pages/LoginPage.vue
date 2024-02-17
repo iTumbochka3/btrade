4cde63d081c8d9130e544f3e855eed47cd1757be
Tumen Dorzhaev <tumen-dorzhaev@mail.ru>
2024-02-17 20:22:16 +0800

    add eslint prettier

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,5 @@
-<template>
-    login
-</template>
+<template>login</template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-</style>
+<style scoped></style>
