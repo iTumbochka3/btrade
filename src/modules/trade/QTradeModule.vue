21e9bd545b12aefe4edc065b70ae88ab5d48ad27
Tumen Dorzhaev <tumen-dorzhaev@mail.ru>
2024-03-30 15:03:20 +0800

    remove test api request

--- src/modules/trade/QTradeModule.vue
+++ src/modules/trade/QTradeModule.vue
@@ -6,15 +6,8 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
 import QTradeChart from "./components/QTradeChart.vue";
 import QTradePanel from "./components/QTradePanel.vue";
-import useAuthStore from "@/store/auth";
-
-const authStore = useAuthStore();
-
-// FIXME: test
-onMounted(() => authStore.login("asdfas3273@sadfasd.ri", "admin"));
 </script>
 
 <style lang="sass" scoped>
